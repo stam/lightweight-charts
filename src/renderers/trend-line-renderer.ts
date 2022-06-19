a36fad13d4906d4efa6f6e4fd70e13a61da8f1dc
Jasper Stam <jasper@strnk.nl>
2022-06-19 14:54:30 +0200

    🔢 Fix hittest for non retarded sort function

--- src/renderers/trend-line-renderer.ts
+++ src/renderers/trend-line-renderer.ts
@@ -52,9 +52,11 @@ export class TrendLineRenderer implements IPaneRenderer {
 			return null;
 		}
 
+		const nonRetardedSortFunction = (a: number, b: number) => a - b;
+
 		// Start and end don't necessarily mean min and max
-		const xRange = [this._data.xStart, this._data.xEnd].sort();
-		const yRange = [this._data.yStart, this._data.yEnd].sort();
+		const xRange = [this._data.xStart, this._data.xEnd].sort(nonRetardedSortFunction);
+		const yRange = [this._data.yStart, this._data.yEnd].sort(nonRetardedSortFunction);
 
 		// Check within massive hitbox
 		if (x < xRange[0] || x > xRange[1] || y < yRange[0] || y > yRange[1]) {
